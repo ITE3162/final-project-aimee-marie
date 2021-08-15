fd51d9b8a812b7ea6b1fc3219edbfc0785b7ae34
princeniyonshuti <npprince47@gmail.com>
2021-08-15 10:58:19 +0200

    connected to postgres db

--- cinemania/cinemania/settings.py
+++ cinemania/cinemania/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'cinemania.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'cinemania',
+        'USER': 'postgres',
+        'PASSWORD': 'lucifer',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
